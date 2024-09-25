014c8824817dbe837536f904808fec6a55d6b1f6
JuanjoGabelo <juan.gabelo27217@ucaldas.edu.co>
2024-09-25 16:01:15 -0500

    Taller Laravel Migrations

---
+++ database/migrations/2024_09_25_185619_create_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 50);
+            $table->integer('phone')->unsigned();
+            $table->integer('age')->unsigned();
+            $table->date('birth_date'); // Tipo date
+            $table->decimal('salary', 8, 2)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
